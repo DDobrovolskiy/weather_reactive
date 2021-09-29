3db595cec5c0ac4f8430f0e34cc753981be8aa1e
DDobrovolskiy <dda.90@mail.ru>
2021-09-29 15:51:25 +0500

    2. Spring boot Webflux [#328882]

---
+++ src/main/java/ru/job4j/weather/ApplicationWeather.java
@@ -0,0 +1,11 @@
+package ru.job4j.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationWeather {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationWeather.class, args);
+    }
+}
